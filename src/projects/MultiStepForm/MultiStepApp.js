3c00062e43d25cb9c3b475c30d977c3752abe991
alexander <world.kor.films@gmail.com>
2022-07-10 15:44:53 +0300

    Multi-Step-Form Done with an indicator (src\projects\MultiStepForm)

--- src/projects/MultiStepForm/MultiStepApp.js
+++ src/projects/MultiStepForm/MultiStepApp.js
@@ -5,7 +5,7 @@ import styles from './MultiStepApp.module.scss'
 import PropTypes from 'prop-types'
 import Button from '@mui/material/Button'
 import { MyStepper } from './components/MyStepper/MyStepper'
-import { mixed, number, object } from 'yup'
+import { mixed, number, object, string } from 'yup'
 
 export const MultiStepApp = () => {
     return (
@@ -17,17 +17,14 @@ export const MultiStepApp = () => {
                 money: 0,
                 description: '',
             }}
-            validationSchema={object({
-                money: mixed().when('millionaire', {
-                    is: true,
-                    then: number()
-                        .required()
-                        .min(1_000_000, 'Because you said you are a millionaire you need to have 1 million'),
-                    otherwise: number().required(),
-                }),
-            })}
         >
-            <div className={styles.step}>
+            <FormikStep
+                className={styles.step}
+                validationSchema={object({
+                    firstName: string().required(),
+                    lastName: string().required(),
+                })}
+            >
                 <Field component={TextField} label="First Name" name="firstName" variant="outlined" />
                 <Field component={TextField} label="Last Name" name="lastName" variant="outlined" />
                 <Field
@@ -36,25 +33,56 @@ export const MultiStepApp = () => {
                     name="millionaire"
                     Label={{ label: 'I am a millionaire' }}
                 />
-            </div>
-            <div className={styles.step}>
-                <Field component={TextField} type="number" label="Amount" name="money" variant="outlined" />
-            </div>
-            <div className={styles.step}>
+            </FormikStep>
+
+            <FormikStep
+                className={styles.step}
+                validationSchema={object({
+                    money: mixed().when('millionaire', {
+                        is: true,
+                        then: number()
+                            .required('Required')
+                            .min(
+                                1_000_000,
+                                'Because you said you are a millionaire you need to have 1 million'
+                            ),
+                        otherwise: number().required(),
+                    }),
+                })}
+            >
+                <Field
+                    component={TextField}
+                    type="number"
+                    label="Amount of money"
+                    name="money"
+                    variant="outlined"
+                />
+            </FormikStep>
+
+            <FormikStep className={styles.step}>
                 <Field component={TextField} label="Description" name="description" variant="outlined" />
-            </div>
+            </FormikStep>
         </FormikStepper>
     )
 }
+const FormikStep = ({ children, validationSchema = {}, ...props }) => {
+    return <div {...props}>{children}</div>
+}
+FormikStep.propTypes = {
+    children: PropTypes.oneOfType([PropTypes.element, PropTypes.arrayOf(PropTypes.element)]),
+    validationSchema: PropTypes.object,
+}
+
 const FormikStepper = ({ children, ...props }) => {
     const childrenArray = React.Children.toArray(children)
     const [step, setStep] = useState(0)
-
+    const currentChild = childrenArray[step]
     const isLastStep = (step, childrenArray) => step === childrenArray.length - 1
 
     return (
         <Formik
             {...props}
+            validationSchema={currentChild.props.validationSchema}
             onSubmit={async (values, helpers) => {
                 if (isLastStep(step, childrenArray)) {
                     console.log('SUBMIT = ', values, helpers)
@@ -67,7 +95,7 @@ const FormikStepper = ({ children, ...props }) => {
                 <div className={styles.flexWrapper}>
                     <div className={styles.centerBlock}>
                         <MyStepper step={step} />
-                        {childrenArray[step]}
+                        {currentChild}
                         {step > 0 && (
                             <Button variant="contained" onClick={() => setStep((prev) => prev - 1)}>
                                 Back
